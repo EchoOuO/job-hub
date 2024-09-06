df1fd2dc04070c3068a76b7912c34fc42882b3ee
EchoOuO <window9995@gmail.com>
2024-09-06 13:33:08 -0700

    feat(client): client can handle query parameters, open specific job information/application modal by id

--- packages/client/src/app/pages/dashboard/services/dashboard.service.ts
+++ packages/client/src/app/pages/dashboard/services/dashboard.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, signal, type WritableSignal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subject, firstValueFrom } from 'rxjs';
 import type { Jobs } from '../../../@types/Jobs';
 import type { PageInfo } from '../../../@types/utils';
 import type { BasePaginationService } from '../../../components/pagination/pagination.type';
 import { JobsApiService } from '../../../core/api/jobs/jobs-api.service';
+import { ApplicationModalComponent } from '../components/application-modal/application-modal.component';
+import { JobInfoModalComponent } from '../components/job-info-modal/job-info-modal.component';
 
 interface JobsFilters {
   applicationStatus?: string | null;
@@ -38,7 +42,16 @@ export class DashboardService implements BasePaginationService {
     archived: false,
   });
 
-  public constructor(private readonly jobsApiService: JobsApiService) {
+  public jobId: string = '';
+
+  public applyJobId: string = '';
+
+  public constructor(
+    private readonly jobsApiService: JobsApiService,
+    private readonly routes: ActivatedRoute,
+    private readonly matDialog: MatDialog,
+    private readonly router: Router,
+  ) {
     this.search$.pipe(takeUntilDestroyed()).subscribe((fromPagination) => {
       void this.searchJobs(fromPagination);
     });
@@ -73,6 +86,18 @@ export class DashboardService implements BasePaginationService {
     this.isLoading.set(false);
     this.pageInfo.set(response.pageInfo);
     this.totalCount.set(response.pageInfo.count);
+
+    const queryParams = this.routes.snapshot.queryParams;
+    this.jobId = queryParams['jobId'];
+    this.applyJobId = queryParams['applyJobId'];
+
+    if (this.jobId && this.applyJobId) {
+      this.openJobApplicationModal(this.jobId);
+    } else if (this.jobId) {
+      this.openJobInfoModal(this.jobId);
+    } else if (this.applyJobId) {
+      this.openJobApplicationModal(this.applyJobId);
+    }
   }
 
   public resetFilters(): void {
@@ -136,5 +161,39 @@ export class DashboardService implements BasePaginationService {
     });
 
     this.isLoading.set(false);
+  }
+
+  public openJobInfoModal(jobId: string) {
+    const data = this.dataSource().find((job) => job.id === jobId);
+
+    if (!data) {
+      alert('The job is not found');
+      void this.router.navigate(['/dashboard']);
+      return;
+    }
+
+    this.matDialog
+      .open(JobInfoModalComponent, {
+        data: { job: data },
+      })
+      .afterClosed()
+      .subscribe(void this.router.navigate(['/dashboard']));
+  }
+
+  public openJobApplicationModal(applyJobId: string) {
+    const data = this.dataSource().find((job) => job.id === applyJobId);
+
+    if (!data) {
+      alert('The job is not found');
+      void this.router.navigate(['/dashboard']);
+      return;
+    }
+
+    this.matDialog
+      .open(ApplicationModalComponent, {
+        data: { job: data },
+      })
+      .afterClosed()
+      .subscribe(void this.router.navigate(['/dashboard']));
   }
 }
